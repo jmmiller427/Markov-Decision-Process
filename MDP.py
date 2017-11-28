4417e04ef6cdac014c315611dc81a47506078bb3
jmmiller427 <jmmi234@g.uky.edu>
2017-11-28 12:50:02 -0500

    Defined t0

--- MDP.py
+++ MDP.py
@@ -5,10 +5,18 @@ Class: CS 463 - 001
 Project: Markov Decision Process
 """
 
+GAMMA = 0.96
+PROB_STAY = 0.1
+PROB_OPPOSITE = 0.2
+PROB_SUCCEED = 0.7
+
 
 def main():
 
-    print("Hello, World.")
+    t0 = [[ 0,  0, 3, 10],
+          [ 0,  5, 0, 60],
+          [ 5, 10, 5,  0],
+          [45,  0, 0,  5]]
 
 
 main()
